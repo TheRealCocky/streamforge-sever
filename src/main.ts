044f77040c3ba1514aae3b0c547f1c9f85f5223e
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-12-06 21:31:18 +0100

    o mesmo erro

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3001',
-      'https://streamsentra.vercel.app/'
+      'https://streamsentra.vercel.app'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
